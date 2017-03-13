2530944ab859bb49367d0c1ad08936b26fba2017
Juani O <juanignacio8ag@gmail.com>
2017-03-13 13:42:11 -0600

    W

---
+++ semaphore.c
@@ -0,0 +1,51 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <asm/system.h>
+#include <asm/atomic.h>
+
+#include <sys/wait.h>
+#include <sys/time.h>
+
+#define atomic_xchg(A,B) __asm__ __volatile__(\
+									"lock xchg %1,%0 ;\n"	\
+									: "=ir" (A)				\
+									: "m" (B), "ir" (A)		\
+									);
+#define CICLOS 10
+
+char *pais[3] = {"Peru", "Bolivia", "Colombia"};
+
+int *g;
+
+struct Semaforo
+{
+	int count;
+};
+
+void waitsem(Semaforo sem) 
+{
+	*sem.count--;
+	if(sem.count < 0)
+	{
+		// agregar proceso a la cola de bloqueados
+	}
+	return;
+}
+
+void signalsem(Semaforo sem) 
+{
+	*sem.count++;
+	if(sem.count <= 0)
+	{
+		//quitar de la cola de bloqueado y agregar a la de listos
+	}
+	return
+}
+
+Semaforo initsem(Semaforo *sem)
+{
+
+}
